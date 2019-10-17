a491ca1668a5a47056743675d7d0aca2aad5b252
Liam Beck <liam.beck@protonmail.com>
2019-10-18 00:42:18 +0200

    added some functionalities

--- main.py
+++ main.py
@@ -7,23 +7,42 @@ class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Convert DEZ")
 
-
-        self.box = Gtk.Box(spacing=6)
-        self.add(self.box)
+        #self.set_default_size(280, 180)
+        self.width = 2
+        grid = Gtk.Grid()
+        self.add(grid)
 
         self.entry = Gtk.Entry()
-        self.box.pack_start(self.entry, True, True, 0)
+        grid.attach(self.entry, 0, 0, self.width, 1)
 
         button = Gtk.Button.new_with_label("Convert")
         button.connect("clicked", self.on_button_convert)
-        self.box.pack_start(button, True, True, 0)
+        grid.attach(button, 0, 1, self.width, 1)
 
         self.table = Gtk.ListStore(str, str)
-        self.show_all()
+        self.table.append(["Binary Code", ""])
+        self.table.append(["Octal Code", ""])
+        self.table.append(["Hexidecimal", ""])
+
+        self.tree = Gtk.TreeView(self.table)
+        renderer = Gtk.CellRendererText()
+
+        column1 = Gtk.TreeViewColumn("First", renderer, text=0)
+        self.tree.append_column(column1)
+
+        column2 = Gtk.TreeViewColumn("Second", renderer, text=1)
+        self.tree.append_column(column2)
+
+        grid.attach(self.tree, 0, 2, self.width, 1)
 
     def on_button_convert(self, button):
-        treeiter = self.table.append(["Binary Code", "010001101"])
-        self.show_all()
+        self.decimal = self.entry.get_text()
+        self.table.clear()
+        self.table.append(["Binary Code", bin(int(self.decimal))])
+        self.table.append(["Octal Code", oct(int(self.decimal))])
+        self.table.append(["Hexidecimal", hex(int(self.decimal))])
+
+
 
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
